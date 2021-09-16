17e06e77452ab286f48a78142c3ef490faa1d76b
Jean Luc <jeanassamoi98@yahoo.com>
2021-09-16 17:08:11 +0200

    commenaires

--- src/main/java/com/askamservices/Test.java
+++ src/main/java/com/askamservices/Test.java
@@ -1,9 +1,12 @@
+/** Commentaires */
+
 package com.askamservices;
 
 public class Test{
 
     public static void main(String[] args){
 
+        //-----------  -------------
         System.out.println("Bonjour le monde!");
     }
 }
